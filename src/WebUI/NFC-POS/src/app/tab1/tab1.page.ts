0486374c394c20617eebd556df07d52487c7ed7a
Amadej Skornšek <skornsek.a@gmail.com>
2022-07-24 13:39:53 +0200

    Aded simple api call

--- src/WebUI/NFC-POS/src/app/tab1/tab1.page.ts
+++ src/WebUI/NFC-POS/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherForecast, WeatherForecastClient } from '../web-api-client';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private client: WeatherForecastClient) {
+    client.get().subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
 
 }
