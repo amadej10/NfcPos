52a7b1581a9eaa44bcfc06adf0c20704ca84856a
skornsek <skornsek.a+github@gmail.com>
2022-12-21 17:02:34 +0100

    Updated Ui

--- src/WebUI/NFC-POS/src/app/tab1/tab1.page.ts
+++ src/WebUI/NFC-POS/src/app/tab1/tab1.page.ts
@@ -1,22 +1,75 @@
-import { Component } from '@angular/core';
-import { WeatherForecast, WeatherForecastClient } from '../web-api-client';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CreateUserCommand, UsersClient, UsersVm, WeatherForecast, WeatherForecastClient } from '../web-api-client';
+// import { NFC, Ndef, NfcTag } from '@awesome-cordova-plugins/nfc/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  @ViewChild(IonModal) modal: IonModal;
 
-  public forecasts: WeatherForecast[] = [];
+  public usersData$: Observable<UsersVm>;
+  public readerMode$: any;
 
-  constructor(private client: WeatherForecastClient) {
-    
+  name: string;
+  surname: string;
+  description: string;
+  balance: number;
+  tag: string;
+
+  // private nfc: NFC, private ndef: Ndef
+  constructor(private userClient: UsersClient,) {
+
+  }
+  ngOnInit(): void {
+    this.getUsersData();
+
+    // let flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
+    // this.readerMode$ = this.nfc.readerMode(flags).subscribe(
+    //   tag => {
+    //     this.tag = JSON.stringify(tag);
+    //     console.log(tag);
+    //   },
+    //   err => console.log('Error reading tag', err)
+    // );
   }
 
-  getData(){
-    this.client.get().subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+
+  getUsersData() {
+    this.usersData$ = this.userClient.getUsers().pipe(map(x => new UsersVm({ users: x.users.sort((a, b) => (a.name > b.name) ? 1 : (b.name > a.name) ? -1 : 0) })));
+  }
+
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+
+    this.userClient.createUser(new CreateUserCommand({
+      name: this.name,
+      surname: this.surname,
+      description: this.description,
+      balance: this.balance
+    })).subscribe((result) => {
+      console.log("user id:", result)
+      this.modal.dismiss(null, 'confirm');
+    },
+      error => {
+        console.error(error);
+      });
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.getUsersData();
+    }
   }
 }
