7be90ac520a766a13ec832eaf8ee3a5e25e86013
skornsek <skornsek.a+github@gmail.com>
2022-12-24 17:45:40 +0100

    Implemented a simple top up

--- src/WebUI/NFC-POS/src/app/tab3/tab3.page.ts
+++ src/WebUI/NFC-POS/src/app/tab3/tab3.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { NfcService } from '../services/nfc.service';
+import { OperationsClient, TopUpCommand, UsersClient, UserVm } from '../web-api-client';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +12,58 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  nfcId: string;
+  user$: Observable<UserVm>;
+  user: UserVm;
+  topUpAmount: number;
+
+  constructor(
+    private nfcService: NfcService,
+    private userClient: UsersClient,
+    private operationsClient: OperationsClient,
+    private toastController: ToastController,
+    private ngZone: NgZone
+  ) {
+
+    nfcService.getId().subscribe((nfcId) => {
+      this.nfcId = nfcId;
+      console.log(nfcId, this.nfcId)
+      if (nfcId) {
+        this.presentToast("NFC tag scanned with id: " + nfcId);
+
+        this.ngZone.run(() => {
+          this.user$ = this.userClient.getUser(nfcId).pipe(tap(x => this.user = x));
+        });
+
+      }
+    })
+  }
+
+  topUp() {
+    this.operationsClient.topUp(new TopUpCommand({ topUpAmount: this.topUpAmount, nfcId: this.nfcId })).subscribe(result => {
+      this.presentToast("New user balance is " + result.newBalance);
+      this.initState();
+      console.log("new balance "+result.newBalance);
+    }, error => {
+      console.error(error);
+    })
+  }
+
+  async presentToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+      position: 'top'
+    });
+    await toast.present();
+  }
+
+  initState() {
+    this.ngZone.run(() => {
+      this.nfcId = undefined;
+      this.topUpAmount = undefined;
+    });
+  }
+
 
 }
