9e6daa3fb452b3b912af087508cbd9239e4cc15b
skornsek <skornsek.a+github@gmail.com>
2022-12-26 20:54:21 +0100

    Added tab 3 not found exception

--- src/WebUI/NFC-POS/src/app/tab3/tab3.page.ts
+++ src/WebUI/NFC-POS/src/app/tab3/tab3.page.ts
@@ -1,7 +1,7 @@
 import { Component, NgZone } from '@angular/core';
 import { ToastController } from '@ionic/angular';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { NfcService } from '../services/nfc.service';
 import { OperationsClient, TopUpCommand, UsersClient, UserVm } from '../web-api-client';
 
@@ -30,7 +30,19 @@ export class Tab3Page {
       console.log(nfcId, this.nfcId)
       if (nfcId) {
         this.ngZone.run(() => {
-          this.user$ = this.userClient.getUser(nfcId).pipe(tap(x => this.user = x));
+          this.user$ = this.userClient.getUser(nfcId)
+            .pipe(
+              tap(x => this.user = x),
+              catchError((err, caught) => {
+                this.nfcId = undefined;
+                let response = JSON.parse(err.response);
+                if (response.status == 404) {
+                  this.presentToast("User with this NFC ID was not found.", 'middle');
+                }
+                console.error(response);
+                console.error(caught);
+                return EMPTY;
+              }));
         });
 
       }
@@ -41,20 +53,21 @@ export class Tab3Page {
     this.operationsClient.topUp(new TopUpCommand({ topUpAmount: this.topUpAmount, nfcId: this.nfcId })).subscribe(result => {
       this.presentToast("New user balance is " + result.newBalance);
       this.initState();
-      console.log("new balance "+result.newBalance);
+      console.log("new balance " + result.newBalance);
     }, error => {
       console.error(error);
     })
   }
 
-  async presentToast(message) {
+  async presentToast(message, pos = 'top' as 'top' | 'bottom' | 'middle') {
     const toast = await this.toastController.create({
       message: message,
       duration: 1500,
-      position: 'top'
+      position: pos
     });
     await toast.present();
   }
+
 
   initState() {
     this.ngZone.run(() => {
@@ -63,7 +76,7 @@ export class Tab3Page {
     });
   }
 
-  cancel(){
+  cancel() {
     this.initState();
   }
 
