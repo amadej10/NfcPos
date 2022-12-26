29672962808102ff008370539260d84af0fab72d
skornsek <skornsek.a+github@gmail.com>
2022-12-26 20:50:02 +0100

    Get user throw 404 if invalid id

--- src/Application/Users/Queries/GetUser/GetUserQuery.cs
+++ src/Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -7,8 +7,10 @@ using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using NfcPos.Application.Common.Exceptions;
 using NfcPos.Application.Common.Interfaces;
 using NfcPos.Application.Users.Queries.Common;
+using NfcPos.Domain.Entities;
 
 namespace NfcPos.Application.Users.Queries.GetUser;
 public class GetUserQuery : IRequest<UserVm>
@@ -32,13 +34,18 @@ public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserVm>
 
     public async Task<UserVm> Handle(GetUserQuery request, CancellationToken cancellationToken)
     {
-        var query = await _context.Users
+        var user = await _context.Users
            .Where(x => x.NfcId == request.nfcId)
            .FirstOrDefaultAsync();
 
+        if (user == null)
+        {
+            throw new NotFoundException(nameof(User), request.nfcId);
+        }
+
         return new UserVm
         {
-            User = _mapper.Map<UserDto>(query)
+            User = _mapper.Map<UserDto>(user)
         };
 
     }
